d999862ad0208b337e243d4e2b679ed8661b1ff1
Lavina91 <65462918+Lavina91@users.noreply.github.com>
2021-02-23 22:12:11 -0700

    added function to view all data on employee table

--- employee_tracker.js
+++ employee_tracker.js
@@ -94,7 +94,11 @@ const viewAllRoles = () => {
 };
 
 const viewAllEmployees = () => {
- 
+    connection.query('SELECT * FROM employee', (err, res) => {
+        if (err) throw err;
+        console.log(res)
+        start();
+    })
  };
 
 const viewByDepartment = () => {
